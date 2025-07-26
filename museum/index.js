84b2f8c64d55ba2390ac0b7761b3bfdc2bdc33cd
mvavilin <realvavilin@yandex.ru>
2025-07-26 16:46:03 +0300

    feat: adapt header for 1024px and add burger menu

--- museum/index.js
+++ museum/index.js
@@ -1,13 +1,24 @@
-const progress_bar = document.querySelector('.control-unit__progress-bar');
+// Прогресс-бар плеера
+const progressBar = document.querySelector(".control-unit__progress-bar");
 
-progress_bar.addEventListener('input', function () {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
-})
+progressBar.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`;
+});
 
-const volume_slider = document.querySelector('.control-unit__volume-slider');
+// Громкость плеера
+const volumeSlider = document.querySelector(".control-unit__volume-slider");
 
-volume_slider.addEventListener('input', function () {
-    const value = this.value;
-    this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`
-})
+volumeSlider.addEventListener("input", function () {
+  const value = this.value;
+  this.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #fff ${value}%, white 100%)`;
+});
+
+// Бургер-меню и навигация
+const burger = document.querySelector(".burger");
+const headerNavigation = document.querySelector(".header__list");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("burger--active");
+  headerNavigation.classList.toggle("header__list--active");
+});

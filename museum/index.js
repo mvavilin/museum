582347f7ab13159ea520c76677b05a1e3d18b0da
mvavilin <realvavilin@yandex.ru>
2025-07-27 15:49:30 +0300

    feat: adapt the Explore section for 1024px and add slider-comparison

--- museum/index.js
+++ museum/index.js
@@ -22,3 +22,18 @@ burger.addEventListener("click", function () {
   burger.classList.toggle("burger--active");
   headerNavigation.classList.toggle("header__list--active");
 });
+
+// Управление шириной верхнего изображения
+const afterImgWrapper = document.querySelector(
+  ".slider-comparison__img-wrapper--after"
+);
+const handleControl = document.querySelector(".slider-comparison__handle");
+const sliderComparisonControl = document.querySelector(
+  ".slider-comparison__control"
+);
+
+sliderComparisonControl.addEventListener("input", () => {
+  const value = sliderComparisonControl.value;
+  afterImgWrapper.style.setProperty("--width-percent", `${value}%`);
+  handleControl.style.setProperty("--width-percent", `${value}%`);
+});

d183630e3c244a01d20eba489c8129ce30af81ab
mvavilin <realvavilin@yandex.ru>
2025-08-07 18:56:44 +0300

    feat: add the function of playing one video in the video slider in the Video section

--- museum/index.js
+++ museum/index.js
@@ -28,63 +28,70 @@ initVideoPlayer();
 // Инициализация анимация при прокрутке изображений в секции Gallery
 initGallery();
 
-
-// const iframe = document.getElementById("videoFrame");
-
-// const originalSrc = iframe.getAttribute("src");
-// const originalSrcdoc = iframe.getAttribute("srcdoc");
-
-// function resetPreview() {
-//   iframe.setAttribute("src", originalSrc);
-//   iframe.setAttribute("srcdoc", originalSrcdoc);
-// }
-
-const videos = [
+// Слайдер в секции Video
+const videosInfo = [
   { videoId: "aWmJ5DgyWPI", poster: "poster0.jpg", title: "Exposition - Le Corps et l'Âme. De Donatello à Michel-Ange. Sculptures italiennes de la Renaissance" },
-  { videoId: "Vi5D6FKhRmo", poster: "poster1.jpg", title: "Au Louvre ! La Vénus de Milo" },
+  { videoId: "2OR0OCr6uRE", poster: "poster1.jpg", title: "Petits contes de Printemps - La ruse du Renard 🦊" },
   { videoId: "NOhDysLnTvY", poster: "poster2.jpg", title: "Promenade dans les collections mésopotamiennes avec Ariane Thomas" },
-  { videoId: "2OR0OCr6uRE", poster: "poster3.jpg", title: "Petits contes de Printemps - La ruse du Renard 🦊" },
+  { videoId: "Vi5D6FKhRmo", poster: "poster3.jpg", title: "Au Louvre ! La Vénus de Milo" },
   { videoId: "zp1BXPX8jcU", poster: "poster4.jpg", title: "Welcome to the Louvre - Bienvenue au Louvre - Musée du Louvre" }
 ];
-
 const videosCont = document.querySelector(".videoslider__videoslides");
+let currentFrame = null;
+let currentPoster = null;
 
-videos.forEach((video, index) => {
-  const videoEl = document.createElement("div");
-  videoEl.className = "videoslider__videoslide videoslide";
-  videoEl.setAttribute("data-index", index);
-  videoEl.innerHTML = `
+function closeCurrentVideo() {
+  if (currentFrame && currentPoster) {
+    currentFrame.replaceWith(currentPoster);
+    currentFrame = null;
+    currentPoster = null;
+  }
+}
+function createPoster(videoInfo, i) {
+  const videoPoster = document.createElement("div");
+  videoPoster.className = "videoslider__videoslide videoslide";
+  videoPoster.setAttribute("data-index", i);
+  videoPoster.innerHTML = `
   <div class="videoslide__wrapper">
     <div class="videoslide__header">
       <div class="videoslide__logo-wrapper">
         <img class="videoslide__logo" src="assets/svg/videoslider/logo.jpg" alt="Channel logo">
       </div>
-      <p class="videoslide__title">${video.title}</p>
+      <p class="videoslide__title">${videoInfo.title}</p>
       <div class="icon videoslide__icon videoslide__icon--kebab"></div>
     </div>
-    <img class="videoslide__preview" src="assets/img/poster/${video.poster}" alt="">
+    <img class="videoslide__preview" src="assets/img/poster/${videoInfo.poster}" alt="">
     <span class="icon videoslide__youtube-icon videoslide__youtube-icon--rect"></span>
     <span class="icon videoslide__youtube-icon videoslide__youtube-icon--pyramid"></span>
   </div>
   `;
+  return videoPoster;
+}
+function createFrame(videoInfo) {
+  const videoFrame = document.createElement("iframe");
+  videoFrame.className = "videoslider__videoslide videoslide";
+  videoFrame.setAttribute("src", `https://www.youtube.com/embed/${videoInfo.videoId}?autoplay=1`);
+  videoFrame.setAttribute("allow", "autoplay; encrypted-media");
+  videoFrame.setAttribute("allowfullscreen", "true");
+  return videoFrame;
+}
+function handlePosterClick(e) {
+  const clickedPoster = e.target.closest(".videoslider__videoslide");
+  if (!clickedPoster) return;
 
-  videosCont.append(videoEl);
-});
-
-videosCont.addEventListener("click", (e) => {
-  const videoEl = e.target.closest(".videoslider__videoslide");
-  if (!videoEl) return;
-
-  const index = videoEl.getAttribute("data-index");
+  const index = clickedPoster.getAttribute("data-index");
   if (index === null) return;
 
-  const video = videos[+index];
+  const videoInfo = videosInfo[+index];
 
-  const iframe = document.createElement("iframe");
-  iframe.className = "videoslider__videoslide videoslide";
-  iframe.setAttribute("src", `https://www.youtube.com/embed/${video.videoId}?autoplay=1`);
-  iframe.setAttribute("allow", "autoplay; encrypted-media");
-  iframe.setAttribute("allowfullscreen", "true");
+  closeCurrentVideo();
 
-  videoEl.replaceWith(iframe);
-});
+  const newVideoFrame = createFrame(videoInfo);
+  clickedPoster.replaceWith(newVideoFrame);
+
+  currentPoster = clickedPoster;
+  currentFrame = newVideoFrame;
+}
+
+videosInfo.forEach((videoInfo, i) => { videosCont.append(createPoster(videoInfo, i)) });
+videosCont.addEventListener("click", handlePosterClick);

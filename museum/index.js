b0bcda0f07c56b0abf2cd98a27f25da04ef19bcd
mvavilin <realvavilin@yandex.ru>
2025-08-10 22:41:28 +0300

    feat: add smooth scrolling up the click

--- museum/index.js
+++ museum/index.js
@@ -7,6 +7,7 @@ import { initVideoslider } from './js/videoslider.js';
 import { initTicketCalculator, saveTicketDateToStorage, restoreTicketDateFromStorageToForm, restoreTicketDateFromStorage } from './js/ticketСalculator.js';
 import { initInteractiveMap } from './js/interactiveMap.js';
 import { initTicketForm, openPopup } from './js/popup.js';
+import { formValidationInitialization } from './js/validation.js';
 
 let play = true;
 const usePlay = (bool) => bool ? play = true : play = false;
@@ -49,119 +50,23 @@ initInteractiveMap();
 // Инициализация калькулятора продажи билетов в форме продажи билетов
 initTicketForm(saveTicketDateToStorage, restoreTicketDateFromStorageToForm, restoreTicketDateFromStorage, usePlay);
 
-// Валидация формы
-document.addEventListener('DOMContentLoaded', () => {
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const phoneInput = document.getElementById('phone');
-  const btnSubmit = document.querySelector('.popup__btn');
+// Инициализация валидации формы
+formValidationInitialization();
 
-  nameInput.addEventListener('input', () => validateName(nameInput));
-  nameInput.addEventListener('blur', () => clearErrorOnInput(nameInput));
-  emailInput.addEventListener('input', () => validateEmail(emailInput));
-  emailInput.addEventListener('blur', () => clearErrorOnInput(emailInput));
-  phoneInput.addEventListener('input', () => validatePhone(phoneInput));
-  phoneInput.addEventListener('blur', () => {
-    formatPhoneInput(phoneInput);
-    clearErrorOnInput(phoneInput);
+// Плавная прокрутка вверх при клике
+const backToTopButton = document.getElementById("returnToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 300) {
+    backToTopButton.classList.add("return-to-top--active");
+  } else {
+    backToTopButton.classList.remove("return-to-top--active");
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
   });
-
-  function validateName(el) {
-    const name = el.value.trim();
-    const errElCont = document.getElementById('nameErrCont');
-    const errEl = document.getElementById('nameErr');
-    const regex = /^[a-zA-Zа-яА-ЯёЁ\s]{3,15}$/;
-    if (!name) {
-      showError(errElCont, errEl, 'This field is required');
-      return false;
-    }
-    if (!regex.test(name)) {
-      showError(errElCont, errEl, 'The name must contain 3-15 characters (letters and spaces only)');
-      return false;
-    }
-    hideError(errElCont, errEl);
-    return true;
-  }
-
-  function validateEmail(el) {
-    const email = el.value.trim();
-    const errElCont = document.getElementById('emailErrCont');
-    const errEl = document.getElementById('emailErr');
-    const regex = /^[a-zA-Z0-9_-]{3,15}@[a-zA-Z]{4,}\.[a-zA-Z]{2,}$/;
-    if (!email) {
-      showError(errElCont, errEl, 'Email is required');
-      return false;
-    }
-    if (!regex.test(email)) {
-      showError(errElCont, errEl, 'Enter email in username@example.com format');
-      return false;
-    }
-    hideError(errElCont, errEl);
-    return true;
-  }
-
-  function validatePhone(el) {
-    const phone = el.value.trim();
-    const errElCont = document.getElementById('phoneErrCont');
-    const errEl = document.getElementById('phoneErr');
-    const digitsOnly = phone.replace(/[^0-9]/g, '');
-    const regex = /^(\d{2,3}[ -]?)+\d{2,3}$|^\d+$/;
-    if (!phone) {
-      showError(errElCont, errEl, 'Phone number is required');
-      return false;
-    }
-    if (digitsOnly.length > 10) {
-      showError(errElCont, errEl, 'Phone number must contain no more than 10 digits');
-      return false;
-    }
-    if (!regex.test(phone)) {
-      showError(errElCont, errEl, 'Phone number must contain only digits, separated by spaces or hyphens');
-      return false;
-    }
-    hideError(errElCont, errEl);
-    return true;
-  }
-
-  function formatPhoneInput(el) {
-    let digits = el.value.replace(/[^0-9]/g, '');
-    if (digits.length > 10) {
-      digits = digits.substring(0, 10);
-    }
-    let formatted = '';
-    digits.length > 3 ? formatted = digits.substring(0, 3) + '-' + digits.substring(3) : formatted = digits;
-    if (formatted.length > 7) {
-      formatted = formatted.substring(0, 7) + '-' + formatted.substring(7);
-    }
-    el.value = formatted;
-  }
-
-  function showError(errElCont, errEl, message) {
-    errElCont.classList.add('booking__input-container--err');
-    errEl.classList.add('booking__err-message--active');
-    btnSubmit.classList.add('popup__btn--inactive');
-    errEl.textContent = message;
-  }
-
-  function hideError(errElCont, errEl) {
-    errElCont.classList.remove('booking__input-container--err');
-    errEl.classList.remove('booking__err-message--active');
-    btnSubmit.classList.remove('popup__btn--inactive');
-  }
-
-  function clearErrorOnInput(el) {
-    const fieldName = el.id;
-    const errElCont = document.getElementById(`${fieldName}ErrCont`);
-    const errEl = document.getElementById(`${fieldName}Err`);
-    hideError(errElCont, errEl);
-  }
-
-  const form = document.querySelector('.popup__form');
-  if (form) {
-    form.addEventListener('submit', (e) => {
-      const isNameValid = validateName(nameInput);
-      const isEmailValid = validateEmail(emailInput);
-      const isPhoneValid = validatePhone(phoneInput);
-      if (!isNameValid || !isEmailValid || !isPhoneValid) { e.preventDefault(); }
-    });
-  }
 });
--- museum/js/interactiveMap.js
+++ museum/js/interactiveMap.js
@@ -1,43 +1,49 @@
 // Интерактивная карта в секции Contacts
 export function initInteractiveMap() {
-    const mapEl = document.getElementById('map');
-    const mapErr = 'Не удалось загрузить карту';
-    if (!L) {
-        mapEl.outerHTML = mapErr;
-        return
-    };
-    const markersCoords = [
-        { lat: 48.86091, lng: 2.3364, active: true },
-        { lat: 48.8602, lng: 2.3333, active: false },
-        { lat: 48.8607, lng: 2.3397, active: false },
-        { lat: 48.8619, lng: 2.3330, active: false },
-        { lat: 48.8625, lng: 2.3365, active: false },
-    ];
-    const defaultCoords = markersCoords[0];
-    let defaultZoom = 17;
-    const maxZoom = 20;
-    const map = L.map('map');
-    map.setView([defaultCoords.lat, defaultCoords.lng], defaultZoom);
-    L.tileLayer(
-        'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
-        {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.carto.com/">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: maxZoom,
-        }
-    ).addTo(map);
-    const getIcon = (isActive) => L.icon({
-        iconUrl: isActive
-            ? 'assets/svg/map/marker-active.svg'
-            : 'assets/svg/map/marker.svg',
-        iconSize: [32, 41],
-        iconAnchor: [16, 41],
-        popupAnchor: [0, -41],
-    });
-    markersCoords.forEach(markerCoords => {
-        const customIcon = getIcon(markerCoords.active);
-        L.marker([markerCoords.lat, markerCoords.lng], { icon: customIcon }).addTo(map);
-    });
-    const link = document.querySelector('a[href="https://leafletjs.com"]');
-    if (link) { link.before('© '); link.textContent = link.textContent.trim() };
+  const mapEl = document.getElementById('map');
+  const mapErr = 'Не удалось загрузить карту';
+  if (!L) {
+    mapEl.outerHTML = mapErr;
+    return
+  };
+  const markersCoords = [
+    { lat: 48.86091, lng: 2.3364, active: true },
+    { lat: 48.8602, lng: 2.3333, active: false },
+    { lat: 48.8607, lng: 2.3397, active: false },
+    { lat: 48.8619, lng: 2.3330, active: false },
+    { lat: 48.8625, lng: 2.3365, active: false },
+  ];
+  const defaultCoords = markersCoords[0];
+  let defaultZoom = 17;
+  const maxZoom = 20;
+  const map = L.map('map');
+  map.setView([defaultCoords.lat, defaultCoords.lng], defaultZoom);
+  L.tileLayer(
+    'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+    {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://www.carto.com/">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: maxZoom,
+    }
+  ).addTo(map);
+  const getIcon = (isActive) => L.icon({
+    iconUrl: isActive
+      ? 'assets/svg/map/marker-active.svg'
+      : 'assets/svg/map/marker.svg',
+    iconSize: [32, 41],
+    iconAnchor: [16, 41],
+    popupAnchor: [0, -41],
+  });
+  markersCoords.forEach(markerCoords => {
+    const customIcon = getIcon(markerCoords.active);
+    L.marker([markerCoords.lat, markerCoords.lng], { icon: customIcon }).addTo(map);
+  });
+  const link = document.querySelector('a[href="https://leafletjs.com"]');
+  if (link) { link.before('© '); link.textContent = link.textContent.trim() };
+  const sourceElement = document.querySelector('.leaflet-top.leaflet-left');
+  const targetElement = document.querySelector('.leaflet-top.leaflet-right');
+  if (sourceElement && targetElement) {
+    targetElement.innerHTML = sourceElement.innerHTML;
+    sourceElement.innerHTML = '';
+  }
 }
